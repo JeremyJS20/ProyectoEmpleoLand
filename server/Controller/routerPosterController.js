2db241c937f355583e471b4b152b8ae30df346d7
JeremyJS20 <55357712+JeremyJS20@users.noreply.github.com>
2022-02-14 01:29:05 -0400

    Update routerPosterController.js

--- server/Controller/routerPosterController.js
+++ server/Controller/routerPosterController.js
@@ -3,7 +3,7 @@ const { response } = require("express");
 const passport = require("passport");
 require("../JSNode/passport")(passport);
 const restler = require('restler');
-const host = 'http://localhost:3000'
+const host = 'https://empleoland-restapi.herokuapp.com/'
 let multiUso = "";
 let multiUso2 = "";
 let date = "";
